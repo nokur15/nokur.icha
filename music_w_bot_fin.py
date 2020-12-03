be540907b86809fa632869e198b0f5159a39d549
nokur15 <naufal.kurniawan.indonesia.111@gmail.com>
2020-12-03 21:44:12 +0700

    Final Version

--- music_w_bot_fin.py
+++ music_w_bot_fin.py
@@ -34,28 +34,16 @@ args = vars(ap.parse_args())
 
 mypath = args["directory"]
 musicfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and f.endswith(".mp3"))]
-players=[]
-#Entering list of songs into OMXPlayer class
-'''
-for i in musicfiles:
-            player_log = logging.getLogger("Player " + str(musicfiles.index(i)))
-            direc = mypath + "/" + i
-            players.append(OMXPlayer(direc, 
-                    dbus_name=('org.mpris.MediaPlayer2.omxplayer' + str(musicfiles.index(i)+1))))
-            players[musicfiles.index(i)].pause()
-            players[musicfiles.index(i)].playEvent += lambda _: player_log.info("Play")
-            players[musicfiles.index(i)].pauseEvent += lambda _: player_log.info("Pause")
-            players[musicfiles.index(i)].stopEvent += lambda _: player_log.info("Stop")
-'''
+
 
 #Detecting motion to play music while also receiving commands from Telegram Bot
 class AutoTrigger():
     #Play the music from musicfiles list
     def call_omxplayer(self):
-        for i in players:
+        for i in self.players:
             self.pause_time = 0
             self.playing = i
-            self.song = musicfiles[players.index(i)]
+            self.song = musicfiles[self.players.index(i)]
             print ("playing " + self.song)
             self.playing.play()
             sleep(self.playing.duration())
@@ -95,19 +83,27 @@ class AutoTrigger():
         print (command)
     
         if command == '/hi':
-            bot.sendMessage (chat_id, str("Welcome to Study Comp!"))
+            bot.sendMessage (chat_id, str("""Welcome to Study Comp!
+Give command "/help" to see the list of commands"""))
         elif command == '/start':
             bot.sendMessage (chat_id, str("Starting the device"))
             self.is_program_running = True
+            self.players = []
+            #Setting GPIO pin 11
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(self.pin, GPIO.IN)
+            #Entering list of songs into OMXPlayer class
             for i in musicfiles:
                 player_log = logging.getLogger("Player " + str(musicfiles.index(i)))
                 direc = mypath + "/" + i
-                players.append(OMXPlayer(direc, 
+                self.players.append(OMXPlayer(direc, 
                             dbus_name=('org.mpris.MediaPlayer2.omxplayer' + str(musicfiles.index(i)+1))))
-                players[musicfiles.index(i)].pause()
-                players[musicfiles.index(i)].playEvent += lambda _: player_log.info("Play")
-                players[musicfiles.index(i)].pauseEvent += lambda _: player_log.info("Pause")
-                players[musicfiles.index(i)].stopEvent += lambda _: player_log.info("Stop")
+                self.players[musicfiles.index(i)].pause()
+                self.players[musicfiles.index(i)].playEvent += lambda _: player_log.info("Play")
+                self.players[musicfiles.index(i)].pauseEvent += lambda _: player_log.info("Pause")
+                self.players[musicfiles.index(i)].stopEvent += lambda _: player_log.info("Stop")
+            #Detecting Motion from PIR Sensor
+            GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=lambda x: self.play_song(), bouncetime=10)
         elif command == '/pause':
             bot.sendMessage (chat_id, str("Pausing the song"))
             bot.sendMessage (chat_id, self.song)
@@ -137,32 +133,34 @@ class AutoTrigger():
             self.new_vol = self.playing.volume() - 1
             self.playing.set_volume(self.new_vol)
         elif command == '/about':
-            bot.sendMessage(chat_id, str("This bot is for LTKA Final Assignment."
-                                    "Created by Thirza Nabila Syafriady and M. Naufal Kurniawan." 
-                                    "This bot will enable users to control Raspberry Pi by Pausing, Playing and Showing the list of songs. "
-                                    "Please do activate the Raspberry Pi before using this Bot."))
+            bot.sendMessage(chat_id, str("""This bot is for LTKA Final Assignment.
+Created by Thirza Nabila Syafriady and M. Naufal Kurniawan.
+This bot will enable users to control Raspberry Pi by Pausing, Playing and Showing the list of songs.
+Please do activate the Raspberry Pi before using this Bot."""))
         elif command == '/help':
-            bot.sendMessage(chat_id, str("List of available commands:"
-                            "/hi : Introduction Message"
-                            "/start : Starting the program"
-                            "/about : Showing Information about the Bot and Project"
-                            "/songlist : Showing list of songs to be played"
-                            "/play : Play the song after being paused"
-                            "/pause : Pause the song after being played"
-                            "/showvol : Showing the volume level (range from 0-10)"
-                            "/decvol : Decreasing the volume by 1"
-                            "/incvol : Increasing the volume by 1"
-                            "/end : Closing the program"))
+            bot.sendMessage(chat_id, str("""List of available commands:
+/hi : Introduction Message
+/start : Starting the program
+/about : Showing Information about the Bot and Project
+/songlist : Showing list of songs to be played
+/play : Play the song after being paused *
+/pause : Pause the song after being played *
+/showvol : Showing the volume level (range from 0-10) *
+/decvol : Decreasing the volume by 1 *
+/incvol : Increasing the volume by 1 *
+/end : Closing the program
+* Commands cannot activate if "/start" hasn't been executed"""))
         elif command == '/end':
             bot.sendMessage(chat_id, str("Ending the device"))
             GPIO.cleanup()
-            for i in players:
+            for i in self.players:
                 i.stop()
                 i.quit()
             self.is_program_running = False
+            self.is_running = False
     
     #initialize class variables, executing Thread to handle, and Thread to play_song and call_omxplayer
-    def __init__(self,pin, bot, init_state):
+    def __init__(self,pin, bot):
         self.pin = pin      #Pin to receive input from PIR Sensor
         self.bot = bot      #Passing bot variable
         self.is_running = False
@@ -170,19 +168,13 @@ class AutoTrigger():
         self.start_time = 0
         self.end_time = 0
         self.pause_time = 0
-        self.is_program_running = init_state
-        GPIO.setup(self.pin, GPIO.IN)
+        self.players = []
         #Creating MessageLoop as the 1st Thread
         MessageLoop(bot=self.bot,handle=self.handle).run_as_thread()
-        
-        #Detecting Motion from PIR Sensor
-        if self.is_program_running:
-            GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=lambda x: self.play_song(), bouncetime=10)
 
 #Main program initialization (pin location, call AutoTrigger, stops program from KeyboardInterrupt)
 def main(bot):
-    GPIO.setmode(GPIO.BOARD)
-    AutoTrigger(11, bot, False)
+    AutoTrigger(11, bot)
     print ("Ready: !")
     print('Listening...')
     try:
